5529f809051cd01c2dc2925da0d57e59213d8b05
Noah Roberts <noahmroberts@gmail.com>
2021-05-07 15:41:44 -0500

    methods added to practice.js

--- src/practice.js
+++ src/practice.js
@@ -6,6 +6,58 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 })
 
-console.log('knex and driver installed correctly');
+ 
+function searchByProductName(searchTerm) {
+ knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .then(result => { console.log(result) })
+}
 
+//searchByProductName('holo')
 
+function paginateProducts(page) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+//paginateProducts(2)
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => { console.log(result) })
+}
+
+//getProductsWithImages()
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then(result => { console.log(result)})
+}
+
+mostPopularVideosForDays(30)
