716ea1a37c413ec0e3f0be45c16f73079225d881
Noah Roberts <noahmroberts@gmail.com>
2021-05-06 20:18:37 -0500

    added knex

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,11 @@
-console.log('hello, practice')
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+console.log('knex and driver installed correctly');
+
+
