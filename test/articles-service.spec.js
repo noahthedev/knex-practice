38fd110335c290f903f1640bcff3e45898cad274
Noah Roberts <noahmroberts@gmail.com>
2021-05-12 10:08:49 -0500

    added delete article test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -42,6 +42,17 @@ describe(`Articles service object`, function() {
         .insert(testArticles)
     })
 
+    it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+      const articleId = 3
+      return ArticlesService.deleteArticle(db, articleId)
+        .then(() => ArticlesService.getAllArticles(db))
+        .then(allArticles => {
+          // copy the test articles array without the "deleted" article
+          const expected = testArticles.filter(article => article.id !== articleId)
+          expect(allArticles).to.eql(expected)
+        })
+    })
+
     it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
       const thirdId = 3
       const thirdTestArticle = testArticles[thirdId - 1]
