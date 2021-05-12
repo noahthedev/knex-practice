68820b7639d085c2738b782c5712425708a40f11
Noah Roberts <noahmroberts@gmail.com>
2021-05-12 09:55:37 -0500

    adding articles test

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,7 +1,16 @@
 const ArticlesService = {
-  getAllArticles() {
-    return 'all the articles!!'
-  }
+  getAllArticles(knex) {
+    return knex.select('*').from('blogful_articles')
+  },
+  insertArticle(knex, newArticle) {
+    return knex
+      .insert(newArticle)
+      .into('blogful_articles')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
 }
 
 module.exports = ArticlesService
